a5bb0dd4e20da9ab1141e370020cb2d67a75daa6
Wying96 <ying.wu@utbm.fr>
2019-11-18 00:40:10 +0100

    changer class client

--- src/main/java/fr/utbm/entity/Client.java
+++ src/main/java/fr/utbm/entity/Client.java
@@ -5,10 +5,145 @@
  */
 package fr.utbm.entity;
 
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.*;
+
+
 /**
  *
  * @author wuying
  */
-public class Client {
+@Entity
+@Table(name = "CLIENT")
+
+public class Client{
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+  
+    @Column(name = "LASTNAME")
+    private String lastname;
+    
+   
+    @Column(name = "FIRSTNAME")
+    private String firstname;
+    
+    
+    @Column(name = "ADDRESS")
+    private String address;
+    // @Pattern(regexp="^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$", message="Invalid phone/fax format, should be as xxx-xxx-xxxx")//if the field contains phone or fax number consider using this annotation to enforce field validation
+   
+    @Column(name = "PHONE")
+    private String phone;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    
+    @Column(name = "EMAIL")
+    private String email;
+    
+    @JoinColumn(name = "COURSE_SESSION_ID", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private CourseSession courseSessionId;
+
+    public Client() {
+    }
+
+    public Client(Integer id) {
+        this.id = id;
+    }
+
+    public Client(Integer id, String lastname, String firstname, String address, String phone, String email) {
+        this.id = id;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public CourseSession getCourseSessionId() {
+        return courseSessionId;
+    }
+
+    public void setCourseSessionId(CourseSession courseSessionId) {
+        this.courseSessionId = courseSessionId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Client)) {
+            return false;
+        }
+        Client other = (Client) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "fr.utbm.entity.Client[ id=" + id + " ]";
+    }
     
 }
--- src/main/java/fr/utbm/entity/Course.java
+++ src/main/java/fr/utbm/entity/Course.java
@@ -5,10 +5,108 @@
  */
 package fr.utbm.entity;
 
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
 /**
  *
  * @author wuying
  */
-public class Course {
+@Entity
+@Table(name = "COURSE")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Course.findAll", query = "SELECT c FROM Course c")})
+public class Course implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 16)
+    @Column(name = "ID")
+    private String id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 128)
+    @Column(name = "TITLE")
+    private String title;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "courseId")
+    private Collection<CourseSession> courseSessionCollection;
+
+    public Course() {
+    }
+
+    public Course(String id) {
+        this.id = id;
+    }
+
+    public Course(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @XmlTransient
+    public Collection<CourseSession> getCourseSessionCollection() {
+        return courseSessionCollection;
+    }
+
+    public void setCourseSessionCollection(Collection<CourseSession> courseSessionCollection) {
+        this.courseSessionCollection = courseSessionCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Course)) {
+            return false;
+        }
+        Course other = (Course) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "fr.utbm.entity.Course[ id=" + id + " ]";
+    }
     
 }
