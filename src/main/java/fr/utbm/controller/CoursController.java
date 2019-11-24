356cd27797e50e4b5908be44fc2316e1ae0e6fed
Czq96 <ohce69cc@gmail.com>
2019-11-24 11:51:34 +0100

    添加多个jsp页面和路径

---
+++ src/main/java/fr/utbm/controller/CoursController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.utbm.controller;
+
+import fr.utbm.entity.Course;
+import fr.utbm.repository.CourseDaoImp;
+import fr.utbm.service.CourseService;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ *
+ * @author c
+ */
+@Controller
+public class CoursController {
+     @RequestMapping(value="/courses",method=RequestMethod.GET) //用于配制url路径
+    public String printHello(ModelMap model){
+//使用Service返回数据成功
+        CourseService courseService = new CourseService();
+        List<Course> cs = courseService.findAll();
+        model.addAttribute("courses",cs);  
+    return "courses";  //hello 是页面模版的名字 hello.jsp
+        
+    }
+}
